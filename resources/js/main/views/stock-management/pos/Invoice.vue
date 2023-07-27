dad8207d4359d2d949dffcbe1f86c732f10fb923
Losemanki6656 <bbiznestg@gmail.com>
2023-07-28 01:58:51 +0500

    saD

--- resources/js/main/views/stock-management/pos/Invoice.vue
+++ resources/js/main/views/stock-management/pos/Invoice.vue
@@ -1,151 +1,107 @@
 <template>
-    <a-modal
-        :visible="visible"
-        :centered="true"
-        :maskClosable="false"
-        :title="$t('common.print_invoice')"
-        width="400px"
-        @cancel="onClose"
-    >
+    <a-modal :visible="visible" :centered="true" :maskClosable="false" :title="$t('common.print_invoice')" width="400px"
+        @cancel="onClose">
         <div id="pos-invoice">
             <div style="max-width: 400px; margin: 0px auto" v-if="order && order.xid">
                 <div class="invoice-header">
-                    <img
-                        class="invoice-logo"
-                        :src="selectedWarehouse.logo_url"
-                        :alt="selectedWarehouse.name"
-                    />
+                    <img class="invoice-logo" :src="selectedWarehouse.logo_url" :alt="selectedWarehouse.name" />
                 </div>
                 <div class="company-details">
                     <h2>{{ selectedWarehouse.name }}</h2>
                     <p class="company-address">
                         {{ selectedWarehouse.address }}
                     </p>
-                    <h4 style="margin-bottom: 0px">
+                    <!-- <h4 style="margin-bottom: 0px">
                         {{ $t("common.phone") }}: {{ selectedWarehouse.phone }}
                     </h4>
-                    <h4>{{ $t("common.email") }}: {{ selectedWarehouse.email }}</h4>
+                    <h4>{{ $t("common.email") }}: {{ selectedWarehouse.email }}</h4> -->
                 </div>
                 <div class="tax-invoice-details">
-                    <h3 class="tax-invoice-title">{{ $t("sales.tax_invoice") }}</h3>
+                    <h3 class="tax-invoice-title"><b>{{ $t("sales.tax_invoice") }}</b></h3>
                     <table class="invoice-customer-details">
                         <tr>
-                            <td style="width: 50%">
-                                {{ $t("sales.invoice") }} &nbsp;&nbsp;&nbsp;&nbsp;:
+                            <td style="width: 60%">
+                                {{ $t("sales.invoice") }} &nbsp;&nbsp;:
                                 {{ order.invoice_number }}
                             </td>
-                            <td style="width: 50%">
+                            <td style="width: 40%">
                                 {{ $t("common.date") }} :
                                 {{ formatDate(order.order_date) }}
                             </td>
                         </tr>
-                        <tr>
+                        <!-- <tr>
                             <td style="width: 50%">
                                 {{ $t("stock.customer") }} : {{ order.user.name }}
                             </td>
                             <td style="width: 50%">
                                 {{ $t("stock.sold_by") }} : {{ order.staff_member.name }}
                             </td>
-                        </tr>
+                        </tr> -->
                     </table>
                 </div>
                 <div class="tax-invoice-items">
                     <table style="width: 100%">
-                        <thead style="background: #eee">
-                            <td style="width: 5%">#</td>
-                            <td style="width: 25%">{{ $t("common.item") }}</td>
-                            <td
-                                :style="{
-                                    width: selectedWarehouse.show_mrp_on_invoice
-                                        ? '10%'
-                                        : '25%',
-                                }"
-                            >
-                                {{ $t("common.qty") }}
+                        <thead>
+                            <!-- <td style="width: 5%">#</td> -->
+                            <td><b>{{ $t("common.item") }}</b></td>
+                            <td>
+                                <b>{{ $t("common.qty") }}</b>
                             </td>
-                            <td
-                                v-if="selectedWarehouse.show_mrp_on_invoice"
-                                :style="{
-                                    width: selectedWarehouse.show_mrp_on_invoice
-                                        ? '20%'
-                                        : '20%',
-                                }"
-                            >
+                            <!-- <td v-if="selectedWarehouse.show_mrp_on_invoice" :style="{
+                                width: selectedWarehouse.show_mrp_on_invoice
+                                    ? '20%'
+                                    : '20%',
+                            }">
                                 {{ $t("product.mrp") }}
                             </td>
-                            <td
-                                :style="{
-                                    width: selectedWarehouse.show_mrp_on_invoice
-                                        ? '20%'
-                                        : '25%',
-                                }"
-                            >
+                            <td :style="{
+                                width: selectedWarehouse.show_mrp_on_invoice
+                                    ? '20%'
+                                    : '25%',
+                            }">
                                 {{ $t("common.rate") }}
-                            </td>
-                            <td
-                                :style="{
-                                    width: selectedWarehouse.show_mrp_on_invoice
-                                        ? '20%'
-                                        : '25%',
-                                    textAlign: 'right',
-                                }"
-                            >
-                                {{ $t("common.total") }}
+                            </td> -->
+                            <td :style="{
+                                textAlign: 'right',
+                            }">
+                                <b>{{ $t("common.total") }}</b>
                             </td>
                         </thead>
                         <tbody>
-                            <tr
-                                class="item-row"
-                                v-for="(item, index) in order.items"
-                                :key="item.xid"
-                            >
-                                <td>{{ index + 1 }}</td>
+                            <tr class="item-row" v-for="(item, index) in order.items" :key="item.xid">
+                                <!-- <td>{{ index + 1 }}</td> -->
                                 <td>{{ item.product.name }}</td>
                                 <td>{{ item.quantity + "" + item.unit.short_name }}</td>
-                                <td v-if="selectedWarehouse.show_mrp_on_invoice">
+                                <!-- <td v-if="selectedWarehouse.show_mrp_on_invoice">
                                     {{ item.mrp ? formatAmountCurrency(item.mrp) : "-" }}
-                                </td>
-                                <td>{{ formatAmountCurrency(item.unit_price) }}</td>
+                                </td> -->
+                                <!-- <td>{{ formatAmountCurrency(item.unit_price) }}</td> -->
                                 <td style="text-align: right">
                                     {{ formatAmountCurrency(item.subtotal) }}
                                 </td>
                             </tr>
+
                             <tr class="item-row-other">
-                                <td
-                                    :colspan="
-                                        selectedWarehouse.show_mrp_on_invoice ? 4 : 3
-                                    "
-                                    style="text-align: right"
-                                >
+                                <td :colspan="2" style="text-align: right">
                                     {{ $t("stock.order_tax") }}
                                 </td>
-                                <td colspan="2" style="text-align: right">
+                                <td colspan="1" style="text-align: right">
                                     {{ formatAmountCurrency(order.tax_amount) }}
                                 </td>
                             </tr>
                             <tr class="item-row-other">
-                                <td
-                                    :colspan="
-                                        selectedWarehouse.show_mrp_on_invoice ? 4 : 3
-                                    "
-                                    style="text-align: right"
-                                >
+                                <td :colspan="2" style="text-align: right">
                                     {{ $t("stock.discount") }}
                                 </td>
-                                <td colspan="2" style="text-align: right">
+                                <td colspan="1" style="text-align: right">
                                     {{ formatAmountCurrency(order.discount) }}
                                 </td>
                             </tr>
                             <tr class="item-row-other">
-                                <td
-                                    :colspan="
-                                        selectedWarehouse.show_mrp_on_invoice ? 4 : 3
-                                    "
-                                    style="text-align: right"
-                                >
+                                <td :colspan="2" style="text-align: right">
                                     {{ $t("stock.shipping") }}
                                 </td>
-                                <td colspan="2" style="text-align: right">
+                                <td colspan="1" style="text-align: right">
                                     {{ formatAmountCurrency(order.shipping) }}
                                 </td>
                             </tr>
@@ -155,20 +111,20 @@
                 <div class="tax-invoice-totals">
                     <table style="width: 100%">
                         <tr>
-                            <td style="width: 30%">
+                            <!-- <td style="width: 30%">
                                 <h3 style="margin-bottom: 0px">
                                     {{ $t("common.items") }}: {{ order.total_items }}
                                 </h3>
-                            </td>
-                            <td style="width: 30%">
+                            </td> -->
+                            <td style="width: 35%">
                                 <h3 style="margin-bottom: 0px">
-                                    {{ $t("common.qty") }}: {{ order.total_quantity }}
+                                    <b>{{ $t("common.qty") }}: {{ order.total_quantity }}</b>
                                 </h3>
                             </td>
-                            <td style="width: 40%; text-align: center">
-                                <h3 style="margin-bottom: 0px">
-                                    {{ $t("common.total") }}:
-                                    {{ formatAmountCurrency(order.total) }}
+                            <td style="width: 65%; text-align: center">
+                                <h3 style="margin-bottom: 0px; text-align: right">
+                                    <b>{{ $t("common.total") }}:
+                                        {{ formatAmountCurrency(order.total) }}</b>
                                 </h3>
                             </td>
                         </tr>
@@ -176,14 +132,14 @@
                 </div>
                 <div class="paid-amount-deatils">
                     <table style="width: 100%">
-                        <thead style="background: #eee">
-                            <td style="width: 50%">{{ $t("payments.paid_amount") }}</td>
-                            <td style="width: 50%">{{ $t("payments.due_amount") }}</td>
+                        <thead>
+                            <td style="width: 50%; text-align: left"><b>{{ $t("payments.paid_amount") }}</b></td>
+                            <td style="width: 50%;text-align: right"><b>{{ $t("payments.due_amount") }}</b></td>
                         </thead>
                         <tbody>
                             <tr class="paid-amount-row">
-                                <td>{{ formatAmountCurrency(order.paid_amount) }}</td>
-                                <td>{{ formatAmountCurrency(order.due_amount) }}</td>
+                                <td style="text-align: left">{{ formatAmountCurrency(order.paid_amount) }}</td>
+                                <td style="text-align: right">{{ formatAmountCurrency(order.due_amount) }}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -192,30 +148,21 @@
                     <table style="width: 100%">
                         <tr style="text-align: center">
                             <td style="width: 100%">
-                                <h4
-                                    style="margin-bottom: 0px"
-                                    v-if="order.order_payments"
-                                >
+                                <h4 style="margin-bottom: 0px" v-if="order.order_payments">
                                     {{ $t("invoice.payment_mode") }}:
-                                    <span
-                                        v-for="currentOrderPayments in order.order_payments"
-                                        :key="currentOrderPayments.xid"
-                                        style="margin-right: 5px"
-                                    >
+                                    <span v-for="currentOrderPayments in order.order_payments"
+                                        :key="currentOrderPayments.xid" style="margin-right: 5px">
                                         {{
                                             formatAmountCurrency(
                                                 currentOrderPayments.amount
                                             )
                                         }}
-                                        (<span
-                                            v-if="
-                                                currentOrderPayments.payment &&
+                                        (<span v-if="currentOrderPayments.payment &&
                                                 currentOrderPayments.payment
                                                     .payment_mode &&
                                                 currentOrderPayments.payment.payment_mode
                                                     .name
-                                            "
-                                        >
+                                                ">
                                             {{
                                                 currentOrderPayments.payment.payment_mode
                                                     .name
@@ -231,10 +178,7 @@
                         </tr>
                     </table>
                 </div>
-                <div
-                    v-if="selectedWarehouse.show_discount_tax_on_invoice"
-                    class="discount-details"
-                >
+                <div v-if="selectedWarehouse.show_discount_tax_on_invoice" class="discount-details">
                     <p>
                         {{ $t("invoice.total_discount_on_mrp") }} :
                         {{ formatAmountCurrency(order.saving_on_mrp) }}
@@ -249,11 +193,8 @@
                     </p>
                 </div>
                 <div class="barcode-details">
-                    <vue-barcode
-                        :value="order.invoice_number"
-                        :options="{ height: 25, width: 1, fontSize: 15 }"
-                        tag="svg"
-                    ></vue-barcode>
+                    <vue-barcode :value="order.invoice_number" :options="{ height: 25, width: 1, fontSize: 15 }"
+                        tag="svg"></vue-barcode>
                 </div>
                 <div class="thanks-details">
                     <h3>{{ $t("invoice.thanks_message") }}</h3>
